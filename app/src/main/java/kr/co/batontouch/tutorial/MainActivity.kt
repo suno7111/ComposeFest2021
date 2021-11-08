a1f2b487c873c248cbbf424c329d8e1d2163c195
suno <suno7111@naver.com>
2021-11-08 19:08:05 +0900

    강의2 이미지 요소 추가

--- app/src/main/java/kr/co/batontouch/tutorial/MainActivity.kt
+++ app/src/main/java/kr/co/batontouch/tutorial/MainActivity.kt
@@ -3,11 +3,14 @@ package kr.co.batontouch.tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import kr.co.batontouch.tutorial.ui.theme.TutorialTheme
 
@@ -24,9 +27,14 @@ data class Message (val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture")
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
     }
 }
 
