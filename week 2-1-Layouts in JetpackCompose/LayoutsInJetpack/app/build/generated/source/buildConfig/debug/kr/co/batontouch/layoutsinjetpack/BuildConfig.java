e9fa188eac82afc7dab937bcadf74ada2fe24acb
suno <suno7111@naver.com>
2021-12-08 17:54:35 +0900

    no message

---
+++ week 2-1-Layouts in JetpackCompose/LayoutsInJetpack/app/build/generated/source/buildConfig/debug/kr/co/batontouch/layoutsinjetpack/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package kr.co.batontouch.layoutsinjetpack;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "kr.co.batontouch.layoutsinjetpack";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
